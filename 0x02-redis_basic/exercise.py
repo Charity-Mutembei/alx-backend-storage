f1409a1d98d787542b28b2be14051812efa1899a
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-21 04:12:29 +0300

    task 2-3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -18,7 +18,51 @@ Remember that data can be a str, bytes, int or float.
 import uuid
 import redis
 from typing import Union, Callable
-from functools import wraps
+import functools
+
+
+@staticmethod
+def call_history(method: Callable) -> Callable:
+    """
+    Familiarize yourself with redis
+    commands RPUSH, LPUSH, LRANGE, etc.
+    """
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+
+        # Append input arguments to the inputs list
+        self._redis.rpush(inputs_key, str(args))
+
+        # Execute the wrapped function to retrieve the output
+        result = method(self, *args, **kwargs)
+
+        # Store the output in the outputs list
+        self._redis.rpush(outputs_key, str(result))
+
+        return result
+
+    return wrapper
+
+
+@staticmethod
+def count_calls(method):
+    """
+    Familiarize yourself with the INCR command and
+    its python equivalent.
+    """
+    counts = {}
+
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        counts[key] = counts.get(key, 0) + 1
+        result = method(self, *args, **kwargs)
+        # print(f"Method {key} has been called {counts} times.")
+        return result
+
+    return wrapper
 
 
 class Cache ():
@@ -41,14 +85,16 @@ class Cache ():
     with the data.
     input data || random key || store them - i guess keys are the ids.
     """
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         as described above
         """
-        random_key = str(uuid.uuid4())
-        self._redis.set(random_key, data)
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
 
-        return random_key
+        return key
 
     def get(self, key: str, fn: callable =
             None) -> Union[str, bytes, int, float, None]:
