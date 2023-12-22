40ffcde61f873f7deec0281cd0e37b40ed970cea
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-23 02:20:00 +0300

    task 10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -18,7 +18,7 @@ def update_topics(mongo_collection, name, topics):
     """
     result = mongo_collection.update_many({'name': name},
                                           {'$set': {'topics': topics}})
-    
+
     """
     Return the number of documents modified
     """
