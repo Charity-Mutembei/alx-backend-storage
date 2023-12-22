bb9df7c25628e3995d9ec99fed7e6ec6c2e07abc
Charity-Mutembei <charity.softwaredev@gmail.com>
2023-12-23 02:15:33 +0300

    task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+a Python function that inserts a new document
+in a collection based on kwargs:
+Prototype: def insert_school(mongo_collection, **kwargs):
+mongo_collection will be the pymongo collection object
+Returns the new _id
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    # Use insert_one to insert a new document into the collection
+    result = mongo_collection.insert_one(kwargs)
+    
+    # Return the new _id
+    return result.inserted_id
